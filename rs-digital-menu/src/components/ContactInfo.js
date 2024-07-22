5397ae6a5a0350effa5d408985bdc4b38a68ed4e
Diego-Oz <ortizdiego1507@gmail.com>
2024-07-21 20:08:24 -0400

    Icons in the contact info component

--- rs-digital-menu/src/components/ContactInfo.js
+++ rs-digital-menu/src/components/ContactInfo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const ContactInfo = ({ icon, text }) => (
+const ContactInfo = ({ icon: Icon, text }) => (
   <div className="flex gap-2 mt-2 text-sm tracking-wide text-stone-500">
-    <img loading="lazy" src={icon} alt="" className="shrink-0 aspect-square w-[17px]" />
+    {Icon && <Icon className="shrink-0 aspect-square w-[18px]" />}
     <p className="flex-auto">{text}</p>
   </div>
 );
