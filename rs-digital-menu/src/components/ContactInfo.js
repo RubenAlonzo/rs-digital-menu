25a1fce31437430984278d0cbf04426fde75efdf
Diego-Oz <ortizdiego1507@gmail.com>
2024-07-21 21:03:12 -0400

    Icon fixes

--- rs-digital-menu/src/components/ContactInfo.js
+++ rs-digital-menu/src/components/ContactInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ContactInfo = ({ icon: Icon, text }) => (
-  <div className="flex gap-2 mt-2 text-sm tracking-wide text-stone-500">
+  <div className="flex items-center gap-2 mt-2 text-sm tracking-wide text-stone-700">
     {Icon && <Icon className="shrink-0 aspect-square w-[18px]" />}
     <p className="flex-auto">{text}</p>
   </div>
